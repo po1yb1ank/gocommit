1fefc86379c5de37fab0812360e273a2ee24a344
Daniil Davydov <daniil.davydov@zecurion.com>
2021-02-09 17:41:34 +0300

    :(:(

--- commit.go
+++ commit.go
@@ -11,50 +11,49 @@ import (
 )
 
 const url = "http://whatthecommit.com/index.txt"
+
 var files = flag.String("f", "nil", "files to add")
-func commit(msg string){
+
+func commit(msg string) {
 	flag.Parse()
-	if *files == "nil"{
+	if *files == "nil" {
 		log.Println("Usage: -f main.go foo.go (files to git add)")
 		os.Exit(1)
 	}
-	switch runtime.GOOS{
+	switch runtime.GOOS {
 	case "windows":
-		err := exec.Command("cmd", "/C","git", "add", *files).Run()
-		if err != nil{
+		err := exec.Command("cmd", "/C", "git", "add", *files).Run()
+		if err != nil {
 			log.Fatal("Something wrong happened. Check your files list", err)
 		}
-		err = exec.Command("cmd", "/C","git", "commit", "-m", msg).Run()
-		if err != nil{
+		err = exec.Command("cmd", "/C", "git", "commit", "-m", msg).Run()
+		if err != nil {
 			log.Fatal(err)
 		}
 	case "linux":
 		err := exec.Command("bash", "git", "add", *files).Run()
-		if err != nil{
+		if err != nil {
 			log.Fatal("Something wrong happened. Check your files list", err)
 		}
 		err = exec.Command("bash", "git", "commit", "-m", msg).Run()
-		if err != nil{
+		if err != nil {
 			log.Fatal(err)
 		}
 	}
 }
-func main(){
+func main() {
 	req, err := http.NewRequest("GET", url, nil)
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 	res, err := http.DefaultClient.Do(req)
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 	defer res.Body.Close()
 	msg, err := ioutil.ReadAll(res.Body)
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 	commit(string(msg))
 }
-
-
-
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/po1yb1ank/gocommit
+
+go 1.15
