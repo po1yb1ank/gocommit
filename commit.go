00985a48f1344fd984d9158477edfb2fe9811b46
Daniil Davydov <daniil.davydov@zecurion.com>
2021-02-09 17:54:40 +0300

    sorry for commits. added branch selection

--- commit.go
+++ commit.go
@@ -13,11 +13,13 @@ import (
 const url = "http://whatthecommit.com/index.txt"
 
 var files = flag.String("f", "nil", "files to add")
+var branch = flag.String("b", "nil", "push to remote branch")
 
 func commit(msg string) {
 	flag.Parse()
-	if *files == "nil" {
+	if *files == "nil" || *branch == "nil" {
 		log.Println("Usage: -f main.go foo.go (files to git add)")
+		log.Println("-b master (remote branch)")
 		os.Exit(1)
 	}
 	switch runtime.GOOS {
@@ -30,12 +32,20 @@ func commit(msg string) {
 		if err != nil {
 			log.Fatal(err)
 		}
+		err = exec.Command("cmd","/C", "git", "push", "origin", *branch).Run()
+		if err != nil {
+			log.Fatal(err)
+		}
 	case "linux":
 		err := exec.Command("bash", "git", "add", *files).Run()
 		if err != nil {
 			log.Fatal("Something wrong happened. Check your files list", err)
 		}
 		err = exec.Command("bash", "git", "commit", "-m", msg).Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = exec.Command("bash", "git", "push", "origin", *branch).Run()
 		if err != nil {
 			log.Fatal(err)
 		}
