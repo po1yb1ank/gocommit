ba4742ace3fbb3126356b3e2b7723bb01e45e56b
Daniil Davydov <daniil.davydov@zecurion.com>
2021-02-15 18:00:26 +0300

    checkout

--- profiler/commands.go
+++ profiler/commands.go
@@ -7,45 +7,50 @@ import (
 	"runtime"
 )
 
-func Add(files string)error{
-	if files == ""{
+//Add git add
+func Add(files string) error {
+	if files == "" {
 		files = "."
 	}
 	switch runtime.GOOS {
 	case "linux", "darwin":
 		err := exec.Command("bash", "git", "add", files).Run()
 		if err != nil {
-			log.Fatal("Something wrong happened while git init.", err)
+			log.Fatal("Something wrong happened while git add.", err)
 			return err
 		}
 	case "windows":
 		err := exec.Command("cmd", "/C", "git", "add", files).Run()
 		if err != nil {
-			log.Fatal("Something wrong happened while git init.", err)
+			log.Fatal("Something wrong happened while git add.", err)
 			return err
 		}
 	}
 	return nil
 }
-func Commit(msg string)error{
+
+//Commit git commit
+func Commit(msg string) error {
 	switch runtime.GOOS {
 	case "linux", "darwin":
 		err := exec.Command("bash", "git", "commit", "-m", msg).Run()
 		if err != nil {
-			log.Fatal("Something wrong happened while git init.", err)
+			log.Fatal("Something wrong happened while git commit.", err)
 			return err
 		}
 	case "windows":
 		err := exec.Command("cmd", "/C", "git", "commit", "-m", msg).Run()
 		if err != nil {
-			log.Fatal("Something wrong happened while git init.", err)
+			log.Fatal("Something wrong happened while git commit.", err)
 			return err
 		}
 	}
 	return nil
 }
-func Init(branch string)error{
-	if branch == ""{
+
+//Init git init
+func Init(branch string) error {
+	if branch == "" {
 		branch = "master"
 	}
 	switch runtime.GOOS {
@@ -64,41 +69,67 @@ func Init(branch string)error{
 	}
 	return nil
 }
-func Push(branch string) error{
-	if branch == ""{
+
+//Push git push
+func Push(branch string) error {
+	if branch == "" {
 		branch = "master"
 	}
 	switch runtime.GOOS {
 	case "linux", "darwin":
 		err := exec.Command("bash", "git", "push", "origin", branch).Run()
 		if err != nil {
-			log.Fatal("Something wrong happened while git init.", err)
+			log.Fatal("Something wrong happened while git push.", err)
 			return err
 		}
 	case "windows":
 		err := exec.Command("cmd", "/C", "git", "push", "origin", branch).Run()
 		if err != nil {
-			log.Fatal("Something wrong happened while git init.", err)
+			log.Fatal("Something wrong happened while git push.", err)
 			return err
 		}
 	}
 	return nil
 }
-func Remote(URL string) error{
-	if URL == ""{
+
+//Remote git remote add
+func Remote(URL string) error {
+	if URL == "" {
 		return fmt.Errorf("empty remote url")
 	}
 	switch runtime.GOOS {
 	case "linux", "darwin":
 		err := exec.Command("bash", "git", "remote", "add", "origin", URL).Run()
 		if err != nil {
-			log.Fatal("Something wrong happened while git init.", err)
+			log.Fatal("Something wrong happened while git remote add.", err)
 			return err
 		}
 	case "windows":
 		err := exec.Command("cmd", "/C", "git", "remote", "add", "origin", URL).Run()
 		if err != nil {
-			log.Fatal("Something wrong happened while git init.", err)
+			log.Fatal("Something wrong happened while git remote add .", err)
+			return err
+		}
+	}
+	return nil
+}
+
+//Checkout git checkout
+func Checkout(branch string) error {
+	if branch == "" {
+		return nil
+	}
+	switch runtime.GOOS {
+	case "linux", "darwin":
+		err := exec.Command("bash", "git", "checkout", "-b", branch).Run()
+		if err != nil {
+			log.Fatal("Something wrong happened while git remote add.", err)
+			return err
+		}
+	case "windows":
+		err := exec.Command("cmd", "/C", "git", "checkout", "-b", branch).Run()
+		if err != nil {
+			log.Fatal("Something wrong happened while git remote add .", err)
 			return err
 		}
 	}
--- profiler/profs.go
+++ profiler/profs.go
@@ -55,7 +55,16 @@ func initProfs() map[string]executor {
 		msg := getCommitMsg()
 
 		/* git add files */
-		err := Add(args[0])
+		err := Checkout(args[1])
+		if err != nil {
+			log.Fatal("Something wrong happened.", err)
+		}
+		log.Println("Switched to: ", args[1])
+		if args[1] == "" {
+			log.Println("default branch")
+		}
+		/* git add files */
+		err = Add(args[0])
 		if err != nil {
 			log.Fatal("Something wrong happened.", err)
 		}
